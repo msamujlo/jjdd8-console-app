d755dd67c8b46912a36e5a656207350d4f7a7aa9
Michal Samujlo <michal.samujlo@gmail.com>
2019-12-17 23:54:31 +0100

    Added some comments

--- src/main/java/com/infoshareacademy/App.java
+++ src/main/java/com/infoshareacademy/App.java
@@ -7,11 +7,16 @@ public class App {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
         String line = "";
+
+        // przygotuj startowy ekran
         ConsoleScreen currentScreen = new MainScreen();
         while (!line.equals("q") && !line.equals("quit")) {
+            // narysuj ekran
             currentScreen.draw();
             line = scanner.nextLine();
+            // na podstawie wejścia użytkownika przygotuj następny ekran
             currentScreen = currentScreen.next(line);
+            // wykonaj akcję następnego ekranu n.p. wyszukaj listę książęk
             currentScreen.action();
         }
     }
